54ff8eb44588313a8943506b6c05cf483feee077
152269205-myseneca <132917070+152269205-myseneca@users.noreply.github.com>
2023-07-26 17:46:59 -0400

    u

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -1,5 +1,5 @@
 # Developed by: Sebastian Maurice, PhD
-# Date: 2023-05-18 
+# Date: 2021-01-18 
 # Toronto, Ontario Canada
 
 # TML python library
@@ -23,7 +23,8 @@ import datetime
 import time
 import os
 
-basedir = os.environ['userbasedir']
+basedir = os.environ['userbasedir'] 
+
 # Set Global Host/Port for VIPER - You may change this to fit your configuration
 VIPERHOST=''
 VIPERPORT=''
@@ -62,9 +63,9 @@ def datasetup(maintopic,preprocesstopic):
      mylocation="Toronto"
 
      # Replication factor for Kafka redundancy
-     replication=1
+     replication=3
      # Number of partitions for joined topic
-     numpartitions=1
+     numpartitions=3
      # Enable SSL/TLS communication with Kafka
      enabletls=1
      # If brokerhost is empty then this function will use the brokerhost address in your
@@ -119,7 +120,7 @@ def sendtransactiondata(maintopic,mainproducerid,VIPERPORT,index,preprocesstopic
 
       # Roll back each data stream by 10 percent - change this to a larger number if you want more data
       # For supervised machine learning you need a minimum of 30 data points in each stream
-     maxrows=2000
+     maxrows=3000
       # Go to the last offset of each stream: If lastoffset=500, then this function will rollback the 
       # streams to offset=500-50=450
      offset=-1
@@ -196,11 +197,17 @@ latlong=lat:long'
      usemysql=1
 
 #     streamstojoin="Current,Voltage,Power"
-     streamstojoin="Current,Voltage,Power"
+     streamstojoin=""
  
      identifier = "IoT device performance and failures"
 
-     preprocesslogic='COUNT,DIFF,SUM'
+     # if dataage - use:dataage_utcoffset_timetype
+     preprocesslogic='max,min,sum,diff,count,avg'
+     #preprocesslogic='dataage_-4_day,trend,min,max' # millisecond,second,minute,hour,day
+     #preprocesslogic='dataage_-4_hour' # millisecond,second,minute,hour,day
+#     preprocesslogic='dataage_1_minute' # millisecond,second,minute,hour,day
+#     preprocesslogic='dataage_1_second' # millisecond,second,minute,hour,day
+#     preprocesslogic='dataage_1_millisecond' # millisecond,second,minute,hour,day
 
      
 #     pathtotmlattrs='oem=id,lat=subject.reference,long=component.0.code.coding.0.display,location=component.1.valueQuantity.value'     
